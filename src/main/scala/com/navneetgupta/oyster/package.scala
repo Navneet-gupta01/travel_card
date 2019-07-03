f2201a1f78b33c4f843dadb91f0ce017ac2274f9
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-07-03 07:36:28 +0400

    changes

--- src/main/scala/com/navneetgupta/oyster/package.scala
+++ src/main/scala/com/navneetgupta/oyster/package.scala
@@ -1,15 +1,15 @@
 package com.navneetgupta
 import zio.ZIO
 
-package object oyster extends CardRepository.Service[CardRepository, Long] with ZonesRepository.Service[ZonesRepository] {
+package object oyster extends CardRepository.Service[CardRepository] with ZonesRepository.Service[ZonesRepository] {
   override def get(cardNumber: Long): ZIO[CardRepository, Nothing, Option[OysterCard[Long]]] =
-    ZIO.accessM(_.cardRepository[Long].get(cardNumber))
+    ZIO.accessM(_.cardRepository.get(cardNumber))
 
   override def create(amount: Option[Double]): ZIO[CardRepository, Nothing, OysterCard[Long]] =
-    ZIO.accessM(_.cardRepository[Long].create(amount))
+    ZIO.accessM(_.cardRepository.create(amount))
 
   override def update(cardNumber: Long, card: OysterCard[Long]): ZIO[CardRepository, Nothing, Option[OysterCard[Long]]] =
-    ZIO.accessM(_.cardRepository[Long].update(cardNumber,card))
+    ZIO.accessM(_.cardRepository.update(cardNumber,card))
 
   override def getZonesByStationCode(code: String): ZIO[ZonesRepository, Nothing, List[Int]] =
     ZIO.accessM(_.zonesRepository.getZonesByStationCode(code))
